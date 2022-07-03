717c7a27a84a4f5fa8657b8caa544429b748f9cc
dezhizhang <1541609448@qq.com>
2022-07-03 21:52:22 +0800

    feat:update

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { useDrag } from 'react-dnd';
+import { useDrag, useDrop } from 'react-dnd';
 import * as types from './types';
 
 const style = {
@@ -10,9 +10,33 @@ const style = {
   cursor: 'move',
 };
 
-function Card({ text, id, index }) {
+function Card({ text, id, index, moveCard }) {
   const ref = useRef();
-  const [{ isDragging },drag] = useDrag({
+
+  useDrop({
+    accept: types.Card,
+    collect: () => ({}),
+    hover(item, monitor) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      const { top, bottom } = ref.current.getBoundingClientRect();
+      const halfOfHoverHeight = (bottom - top) / 2;
+      const { y } = monitor.getClientOffset();
+      const hoverClientY = y - top;
+      if (
+        (dragIndex < hoverIndex && hoverClientY > halfOfHoverHeight) ||
+        (dragIndex > hoverIndex && hoverClientY < halfOfHoverHeight)
+      ) {
+        moveCard(dragIndex, hoverIndex);
+        item.index = hoverIndex;
+      }
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
     type: types.Card,
     item: () => ({ id, index }),
     collect: (monitor) => ({
@@ -21,7 +45,7 @@ function Card({ text, id, index }) {
   });
   const opacity = isDragging ? 0.1 : 1;
   drag(ref);
-  
+
   return (
     <div ref={ref} style={{ ...style, opacity }}>
       {text}
--- src/Container.js
+++ src/Container.js
@@ -1,24 +1,38 @@
 import { useState } from 'react';
 import Card from './Card';
 function Container() {
-  const [list, setList] = useState([
+  const [cards, setCards] = useState([
     {
       text: '卡片1',
       id: 1,
     },
     {
       text: '卡片2',
-      id: 1,
+      id: 2,
     },
     {
       text: '卡片3',
-      id: 1,
+      id: 3,
     },
   ]);
+
+  const moveCard = (dragIndex, hoverIndex) => {
+    const dragCard = cards[dragIndex];
+    const cloneCards = [...cards];
+    cloneCards.slice(dragIndex, 1);
+    cloneCards.splice(hoverIndex, 0, dragCard);
+    setCards(cloneCards);
+  };
   return (
     <div style={{ width: 300 }}>
-      {list.map((item, index) => (
-        <Card key={item.id} text={item.text} id={item.id} index={index} />
+      {cards.map((item, index) => (
+        <Card
+          moveCard={moveCard}
+          key={item.id}
+          text={item.text}
+          id={item.id}
+          index={index}
+        />
       ))}
     </div>
   );
